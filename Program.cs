21c0c0eeadc553501f638c925ab800f01485237b
Leonardo buta <teste@teste.com>
2024-01-17 21:00:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using AprovadoOuReprovado;
+using System;
+using System.Globalization;
+
+Aluno aluno = new Aluno();
+
+Console.Write("Nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+Console.WriteLine("Digite as três notas do aluno:");
+aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal());
+
+if (aluno.Aprovado())
+{ Console.WriteLine("APROVADO");
+}
+
+
+else 
+{ Console.WriteLine("REPROVADO");
+    Console.WriteLine("FALTARAM " + aluno.NotaRestante() + " PONTOS"); 
+}
+
+
